ac72366e2bba4f5e6fe5c96676e5f77f67e99fa4
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2023-09-14 09:25:50 +0200

    Add files via upload

---
+++ CA20230914/CA20230914/Kategoria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CA20230914
+{
+    class Versenyzo
+    {
+        public string Nev { get; set; }
+        public bool Kategoria { get; set; }
+        public string EgyesuletNeve { get; set; }
+        public int[] Pontszamok { get; set; }
+        
+        public int Oszpont
+        {
+            get 
+            {
+                var rendezett = Pontszamok.OrderBy(x => x).ToArray();
+                int op = 0;
+
+                if (rendezett[0] > 0)
+                    op += 10;
+                if (rendezett[1] > 0)
+                    op += 10;
+
+                for (int i = 2; i < rendezett.Length; i++)
+                {
+                    op += rendezett[i];
+                }
+
+                return op;
+            }
+        }
+
+        public Versenyzo(string sor)
+        {
+            string[] k = sor.Split(';');
+            Nev = k[0];
+            Kategoria = k[1] == "Felnott ferfi";
+            EgyesuletNeve = k[2] == "n.a." ? null : k[2];
+            Pontszamok = new int[8];
+            for (int i = 0; i < Pontszamok.Length; i++)
+            {
+                Pontszamok[i] = int.Parse(k[i + 3]);
+            }
+            
+        }
+    }
+}
---
+++ CA20230914/CA20230914/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace CA20230914
+{
+    class Program
+    {
+        static void Main()
+        {
+            var versenyzok = new List<Versenyzo>();
+           using var sr = new StreamReader(
+                path:"fob2016.txt",
+                encoding: Encoding.UTF8
+                );
+
+            while (!sr.EndOfStream)
+            {
+                versenyzok.Add(new Versenyzo(sr.ReadLine()));
+            }
+
+            sr.Close();
+
+            Console.WriteLine($"3. feladat: Versenyzők száma: {versenyzok.Count}");
+
+            int nokSzama = versenyzok.Count(v => !v.Kategoria);
+            float nokAranya = nokSzama / (float)versenyzok.Count * 100;
+            Console.WriteLine($"4. feladat: Női versenyzők aránya: {nokAranya:0.00}%");
+
+            var noiBajnok = versenyzok
+                .Where(x => !x.Kategoria)
+                .OrderBy(v => v.Oszpont)
+                .Last();
+
+            Console.WriteLine("6.feladat: A bajnok női versenyző: ");
+            Console.WriteLine($"\tNév: {noiBajnok.Nev}");
+            Console.WriteLine($"\tEgyesület: {noiBajnok.EgyesuletNeve}");
+            Console.WriteLine($"\tÖsszpont: {noiBajnok.Oszpont}");
+
+            var sw = new StreamWriter(
+                path: "osszpontFF.txt",
+                append: false,
+                encoding: Encoding.UTF8
+                );
+
+            foreach (var v in versenyzok)
+            {
+                if (v.Kategoria) sw.WriteLine($"{v.Nev};{v.Oszpont}");
+            }
+
+            sw.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
